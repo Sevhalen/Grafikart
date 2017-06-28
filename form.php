6e7cdd74e76ec865a540040064baa1a6cc6d8fcd
Sevhalen <sevhalen@gmail.com>
2017-06-28 22:01:35 +0200

    5_0_0 depuis PCP

--- form.php
+++ form.php
@@ -5,7 +5,7 @@ class Form{
     private $data;
     private $surround = 'p';
     
-    public function __construct($data) {
+    public function __construct($data = array()) {
         $this->data = $data;
     }
     
@@ -13,8 +13,12 @@ class Form{
     return "<{$this->surround}>{$html}<{$this->surround}>";
     }
     
+    private function getValue($index) {
+        return isset($this->data[$index]) ? $this->data[$index] : null;
+    }
+    
     public function input($name) {
-        return $this->surround('<input type="text" name="' . $name . '"');
+        return $this->surround('<input type="text" name="'. $name . '" value="' . $this->getValue($name) . '"');
     }
     
     public function submit() {
